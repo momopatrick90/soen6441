bba0ef5d63a4d0ac7906c8b99011d092c77de4dd
therazakhan <L080221@gmail.com>
2015-07-10 20:21:04 -0400

    Main Application
    Main Structure

--- eclipse_project/src/eclipse_project/LanternsApplication.java
+++ eclipse_project/src/eclipse_project/LanternsApplication.java
@@ -2,4 +2,42 @@ package eclipse_project;
 
 public class LanternsApplication {
 
+	
+	public static void main(String args[]){
+		
+
+		int resp = '\0';
+
+		do{
+			
+			System.out.println("Choose an option:");
+			System.out.println("1.New Game");
+			System.out.println("2.Existing Game");
+			Scanner in = new Scanner(System.in);
+			int choice = in.nextInt();
+			
+			if (choice == 1) {
+				loadNewGame();
+			
+			} else if (choice == 2) {
+				
+				loadExistingGame();
+
+			}
+
+			System.out.println("press 9 to continue and ZERO to quit!");
+			in = new Scanner(System.in);
+			resp = in.nextInt();
+		
+		}while(resp != 0); 
+
+	}
+	
+	public static void loadNewGame(){
+		
+	}
+	
+	public static void loadExistingGame(){
+		
+	}
 }
