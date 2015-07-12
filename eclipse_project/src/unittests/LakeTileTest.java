ff13dd362a44f629055b69556d7158bc2e704ef7
sushilApp <patilsushil1991@gmail.com>
2015-07-12 17:23:59 -0400

    Added few test cases
    Test tiles according to number of users

--- eclipse_project/src/unittests/LakeTileTest.java
+++ eclipse_project/src/unittests/LakeTileTest.java
@@ -2,17 +2,52 @@ package unittests;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import eclipse_project.LakeTiles;
 
 public class LakeTileTest {
 
+	LakeTiles lT;
+	@Before
+	public void lakeTileTestClass(){
+		lT=new LakeTiles();
+	}
 	@Test
-	public void lakeTilesForTwoPlayersShouldBeSixteen() {
-		LakeTiles lakeTiles=new LakeTiles();
-		//lakeTiles.initializeLakeTiles(2);
-		assertTrue(lakeTiles.lakeTiles.length==16);
+	public void lakeTilesForTwoPlayersShouldBeTweentyThree() {
+		lT.initializeLakeTiles(2);
+		assertEquals(23,lT.lakeTiles.length);
+	}
+	@Test
+	public void lakeTilesForThreePlayersShouldBeTweentyEight() {
+		lT.initializeLakeTiles(3);
+		assertEquals(28,lT.lakeTiles.length);
+	}
+	@Test
+	public void lakeTilesForFourPlayersShouldBeThirtyThree() {
+		lT.initializeLakeTiles(4);
+		assertEquals(33,lT.lakeTiles.length);
+	}
+	@Test
+	public void testThreeTilesAreAssignedForEachPlayer(){
 		
 	}
-
+	
+	@Test
+	public void testPlaceTile(){
+		//assertFalse(lT.placeTile(26, 30, gameBoard, tileToPlace));
+	}
+	
+	@Test
+	public void testRandomValuesAreBetweenFiveAndEleven(){
+		
+		for(int i=0;i<8;i++){
+			
+		}
+	}
+	
+	
 }
