9ea11a18e73b08dd4c2d18e83d68aaa1f9c71aad
sushilApp <patilsushil1991@gmail.com>
2015-07-13 03:52:49 -0400

    Added few test cases

--- eclipse_project/src/unittests/LakeTileTest.java
+++ eclipse_project/src/unittests/LakeTileTest.java
@@ -2,6 +2,7 @@ package unittests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Assert;
@@ -33,11 +34,24 @@ public class LakeTileTest {
 	}
 	@Test
 	public void testThreeTilesAreAssignedForEachPlayer(){
+		lT.initializeLakeTiles(3);
+		ArrayList<LakeTiles> listTiles =lT.assignLakeTiles(3);
+		LakeTiles tile = new LakeTiles();
+		assertNotNull(listTiles);
 		
+		for(int i = 0; i<3 ; i++){
+			tile = listTiles.get(i);
+			assertNotNull(tile);
+			
+		}
+		
+		//System.out.println(lT.globalLakeTiles.size());
 	}
 	
 	@Test
 	public void testPlaceTile(){
+		lT.initializeLakeTiles(4);
+		//lT.placeTile();
 		//assertFalse(lT.placeTile(26, 30, gameBoard, tileToPlace));
 	}
 	
