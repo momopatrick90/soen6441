3791a7c101fbeb646fdaa0a0ef90de020530f9b3
sushilApp <patilsushil1991@gmail.com>
2015-07-13 03:55:09 -0400

    New test cases

--- eclipse_project/src/unittests/DedicationTokensTest.java
+++ eclipse_project/src/unittests/DedicationTokensTest.java
@@ -56,6 +56,51 @@ public class DedicationTokensTest {
 	
 	}
 	
+	@Test
+	public void testThreePairCount() {
+		
+		DedicationTokens dedicationTokens = new DedicationTokens(4);
+		
+		int tokenExpected=dedicationTokens.threePairCount();
+		assertEquals(9, tokenExpected);
 	
+	}
 	
+	@Test
+	public void testSevenOfUniqueWhenCardsAreAvailable() {
+		
+		DedicationTokens dedicationTokens = new DedicationTokens(4);
+			
+		assertTrue(dedicationTokens.getSevenUnique() > 0);
+	}
+	
+	@Test
+	public void testSevenOfUniqueWhenCardsAreNotAvailable() {
+		
+		DedicationTokens dedicationTokens = new DedicationTokens(4);
+			
+		for(int i=0;i<100;i++)
+			dedicationTokens.getSevenUnique();
+			
+		assertFalse(dedicationTokens.getSevenUnique() > 0);
+	}
+	
+	@Test
+	public void testSevenUniqueCount() {
+		
+		DedicationTokens dedicationTokens = new DedicationTokens(4);
+		
+		int tokenExpected=dedicationTokens.getSevenUnique();
+		assertEquals(10, tokenExpected);
+	
+	}	
+	@Test
+	public void testGenericFourCount() {
+		
+		DedicationTokens dedicationTokens = new DedicationTokens(4);
+		
+		int tokenExpected=dedicationTokens.genericFourCount();
+		assertEquals(3, tokenExpected);
+	
+	}	
 }
