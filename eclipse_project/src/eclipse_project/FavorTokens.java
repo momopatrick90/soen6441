dffa03b2bdd9e9353d0cb27f00176d3458db84d0
therazakhan <L080221@gmail.com>
2015-07-14 01:31:27 -0400

    Stable Code
    code sharing

--- eclipse_project/src/eclipse_project/FavorTokens.java
+++ eclipse_project/src/eclipse_project/FavorTokens.java
@@ -1,50 +1,52 @@
 package eclipse_project;
+
 import java.util.Stack;
 
 /**
- * Favor Tokens - Tokens can be replaced with Lanterns card during the game play.
+ * Favor Tokens - Tokens can be replaced with Lanterns card during the game
+ * play.
  *
  */
 public class FavorTokens {
-	
+
 	private final int MAX_TOKENS = 20;
 	private Stack<String> tokens = new Stack<String>();
-	
+
 	/**
 	 * initializing the favor tokens
 	 */
 	public FavorTokens(int tokenCount) {
-		
-		if(tokenCount < 0 || tokenCount > MAX_TOKENS)
+
+		if (tokenCount < 0 || tokenCount > MAX_TOKENS)
 			tokenCount = 0;
-		
-		for(int i=0; i< tokenCount; i++)
-		{
+
+		for (int i = 0; i < tokenCount; i++) {
 			tokens.push("favor_token");
 		}
 	}
-	
+
 	/**
-	 * Number of favor tokens 
+	 * Number of favor tokens
+	 * 
 	 * @return number of favor tokens available
 	 */
 	public int getTokens() {
 		return tokens.size();
 	}
-	
+
 	/**
 	 * Decrement the favor token
 	 */
-	public void  decrementToken(){
-		if(!this.tokens.isEmpty())
+	public void decrementToken() {
+		if (!this.tokens.isEmpty())
 			this.tokens.pop();
 	}
-	
+
 	/**
 	 * Increment the favor token - Not exceeding the MAX Count
 	 */
-	public void  incrementToken(){
-		if(this.tokens.size() < MAX_TOKENS)
+	public void incrementToken() {
+		if (this.tokens.size() < MAX_TOKENS)
 			this.tokens.push("favor_token");
 	}
 
