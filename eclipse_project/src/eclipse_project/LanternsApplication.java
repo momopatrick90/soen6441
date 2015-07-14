9ed8c53caa1d6d34bb658fae202af1c40fe5b642
therazakhan <L080221@gmail.com>
2015-07-14 01:31:49 -0400

    stable code
    code sharing

--- eclipse_project/src/eclipse_project/LanternsApplication.java
+++ eclipse_project/src/eclipse_project/LanternsApplication.java
@@ -13,9 +13,9 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.*;
 
-
 /**
- *	Main application container - loads the new game or existing game based on the user input
+ * Main application container - loads the new game or existing game based on the
+ * user input
  */
 
 public class LanternsApplication {
@@ -24,29 +24,31 @@ public class LanternsApplication {
 	private static int numOfPlayers;
 	private static int playerTurn;
 	private static int tokensCount;
-	private static int redCount, blueCount, greenCount, whiteCount, purpleCount, blackCount, orangeCount;
-	
-	public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException{
+	private static int redCount, blueCount, greenCount, whiteCount,
+			purpleCount, blackCount, orangeCount;
+
+	public static void main(String[] args) throws ParserConfigurationException,
+			SAXException, IOException {
 
 		int resp = '\0';
 
-		do{
-			
+		do {
+
 			System.out.println("Choose an option:");
 			System.out.println("Press 1 for New Game");
 			System.out.println("Press 2 for Existing Game");
 			System.out.println("Press ZERO to Exit Game");
 			Scanner in = new Scanner(System.in);
 			int choice = in.nextInt();
-			
+
 			if (choice == 1) {
 				System.out.println("Enter number of players");
 				in = new Scanner(System.in);
 				numOfPlayers = in.nextInt();
 				loadNewGame();
-			
+
 			} else if (choice == 2) {
-				
+
 				System.out.println("Enter number of players");
 				in = new Scanner(System.in);
 				String fileName = in.nextLine();
@@ -57,19 +59,20 @@ public class LanternsApplication {
 			System.out.println("press 9 to continue and ZERO to quit!");
 			in = new Scanner(System.in);
 			resp = in.nextInt();
-		
-		}while(resp != 0); 
+
+		} while (resp != 0);
 
 	}
-	
-	public static void loadNewGame(){
-		
+
+	public static void loadNewGame() {
+
 		game = new GameEngine(numOfPlayers);
 		game.startNewGame();
 	}
-	
-	public static void loadExistingGame(String fileName) throws ParserConfigurationException, SAXException, IOException{
-		
+
+	public static void loadExistingGame(String fileName)
+			throws ParserConfigurationException, SAXException, IOException {
+
 		game = new GameEngine();
 		game.loadExistingGame(fileName);
 	}
