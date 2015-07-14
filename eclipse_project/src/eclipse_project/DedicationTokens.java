c3b648a9c2db953954686962e0ef0191260108d3
Muhammad Raza Khan <muhammadrazakhan@Muhammads-MacBook-Pro.local>
2015-07-14 00:26:37 -0400

    dedication tokens
    code sharing

--- eclipse_project/src/eclipse_project/DedicationTokens.java
+++ eclipse_project/src/eclipse_project/DedicationTokens.java
@@ -3,7 +3,8 @@ package eclipse_project;
 import java.util.Stack;
 
 /**
- * Dedication Token - Four of kind, Three Pair and Seven unique colors can be replaced with honor value.
+ * Dedication Token - Four of kind, Three Pair and Seven unique colors can be
+ * replaced with honor value.
  *
  */
 public class DedicationTokens {
@@ -12,14 +13,16 @@ public class DedicationTokens {
 	private Stack<Integer> threePair = new Stack<Integer>();
 	private Stack<Integer> sevenUnique = new Stack<Integer>();
 	private Stack<Integer> genericFour = new Stack<Integer>();
-	
+
 	private final int FOUR_KIND_MAX = 8;
 	private final int THREE_PAIR_MAX = 9;
 	private final int SEVEN_UNIQUE_MAX = 10;
 	private final int GENERIC_FOUR_MAX = 4;
-	
+
 	/**
-	 * Constructor to initialize the Dedication Tokens that depends on the number of players at the start of the game.
+	 * Constructor to initialize the Dedication Tokens that depends on the
+	 * number of players at the start of the game.
+	 * 
 	 * @param numberOfPlayers - Number of players playing game.
 	 */
 	public DedicationTokens(int numberOfPlayers)
@@ -92,137 +95,112 @@ public class DedicationTokens {
 		}
 		
 	}
-	
+
 	/**
-	 * Constructor to initialize the Dedication Tokens while loading the saved game.
+	 * Constructor to initialize the Dedication Tokens while loading the saved
+	 * game.
+	 * 
 	 * @param numberOfPlayers - Number of players playing game.
 	 * @param fourOfKindCount - Number of fourOfKindCount Dedication cards.
-	 * @param threePairCount - Number of threePairCount Dedication cards.
-	 * @param sevenUniqueCount -  Number of sevenUniqueCount Dedication cards.
-	 * @param genericFourCount -  Number of genericFourCount Dedication cards.
+	 * @param threePairCount -  Number of threePairCount Dedication cards.
+	 * @param sevenUniqueCount - Number of sevenUniqueCount Dedication cards.
+	 * @param genericFourCount - Number of genericFourCount Dedication cards.
 	 */
-	public DedicationTokens(int numberOfPlayers,int fourOfKindCount,int threePairCount,int sevenUniqueCount,int genericFourCount) {
-	
-		switch(numberOfPlayers)
-		{
-			case 2:
-				System.out.println("Will be implemented in build 2.Right now all the dedicationTokens will be used");
-				break;
-			case 3:
-				System.out.println("Will be implemented in build 2.Right now all the dedicationTokens will be used");
-				break;
-			case 4:
-				
-				if(fourOfKindCount >= 1 && fourOfKindCount <= 9)
-				{
-					for(int i=1;i<=fourOfKindCount;i++)
-						if(i==9)
-							this.fourOfKind.push(8);
-						else
-							this.fourOfKind.push(i);
-				}
-				
-				if(threePairCount >= 1 && threePairCount <= 9)
-				{
-					for(int i=1;i<=threePairCount;i++)
-						this.threePair.push(i);
-				}
-				
-				if(sevenUniqueCount >= 1 && sevenUniqueCount <= 9)
-				{
-					for(int i=1;i<=sevenUniqueCount;i++)
-						this.sevenUnique.push(i);
-				}
+	public DedicationTokens(int numberOfPlayers, int fourOfKindCount,
+			int threePairCount, int sevenUniqueCount, int genericFourCount) {
 
-				if(genericFourCount >= 1 && genericFourCount <= 3)
-				{
-					for(int i=1; i<=genericFourCount;i++)
-						this.genericFour.push(GENERIC_FOUR_MAX);
-				}
-					
+		if (fourOfKindCount >= 1 && fourOfKindCount <= 9) {
+			for (int i = 1; i <= fourOfKindCount; i++)
+				if (i == 9)
+					this.fourOfKind.push(8);
+				else
+					this.fourOfKind.push(i);
 		}
+
+		if (threePairCount >= 1 && threePairCount <= 9) {
+			for (int i = 1; i <= threePairCount; i++)
+				this.threePair.push(i);
+		}
+
+		if (sevenUniqueCount >= 1 && sevenUniqueCount <= 9) {
+			for (int i = 1; i <= sevenUniqueCount; i++)
+				this.sevenUnique.push(i);
+		}
+
+		if (genericFourCount >= 1 && genericFourCount <= 3) {
+			for (int i = 1; i <= genericFourCount; i++)
+				this.genericFour.push(GENERIC_FOUR_MAX);
+		}
+
 	}
-	
+
 	/**
-	 * method to give one dedicationToken from FourOfKind Stack with its appropriate current value 
+	 * method to give one dedicationToken from FourOfKind Stack with its
+	 * appropriate current value
+	 * 
 	 * @return honor value
 	 */
-	public int getFourOfKind()
-	{
-		int result=0;
-		if(fourOfKind.isEmpty())
-		{
-			if(!genericFour.isEmpty())
-			{
-				result=genericFour.pop();
+	public int getFourOfKind() {
+		int result = 0;
+		if (fourOfKind.isEmpty()) {
+			if (!genericFour.isEmpty()) {
+				result = genericFour.pop();
 			}
-		}
-		else
-		{
-			result=fourOfKind.pop();
+		} else {
+			result = fourOfKind.pop();
 		}
 		return result;
 	}
-	
-	public int fourOfKindCount()
-	{
+
+	public int fourOfKindCount() {
 		return this.fourOfKind.size();
 	}
-	
+
 	/**
-	 * method to give one dedicationToken from threePair Stack with its appropriate current value
+	 * method to give one dedicationToken from threePair Stack with its
+	 * appropriate current value
+	 * 
 	 * @return honor value
 	 */
-	public int getThreePair()
-	{
-		int result=0;
-		if(threePair.isEmpty())
-		{
-			if(!genericFour.isEmpty())
-			{
-				result=genericFour.pop();
+	public int getThreePair() {
+		int result = 0;
+		if (threePair.isEmpty()) {
+			if (!genericFour.isEmpty()) {
+				result = genericFour.pop();
 			}
-		}
-		else
-		{
-			result=threePair.pop();
-		}
-		return result;
-	}
-	
-	public int threePairCount()
-	{
-		return this.threePair.size();
-	}
-	
-	/**
-	 * method to give one dedicationToken from sevenUnique Stack with its appropriate current value.
-	 * @return honor value
-	 */
-	public int getSevenUnique()
-	{
-		int result=0;
-		if(sevenUnique.isEmpty())
-		{
-			if(!genericFour.isEmpty())
-			{
-				result=genericFour.pop();
-			}
-		}
-		else
-		{
-			result=sevenUnique.pop();
+		} else {
+			result = threePair.pop();
 		}
 		return result;
 	}
 
-	public int sevenUniqueCount()
-	{
+	public int threePairCount() {
+		return this.threePair.size();
+	}
+
+	/**
+	 * method to give one dedicationToken from sevenUnique Stack with its
+	 * appropriate current value.
+	 * 
+	 * @return honor value
+	 */
+	public int getSevenUnique() {
+		int result = 0;
+		if (sevenUnique.isEmpty()) {
+			if (!genericFour.isEmpty()) {
+				result = genericFour.pop();
+			}
+		} else {
+			result = sevenUnique.pop();
+		}
+		return result;
+	}
+
+	public int sevenUniqueCount() {
 		return this.sevenUnique.size();
 	}
-	
-	public int genericFourCount()
-	{
+
+	public int genericFourCount() {
 		return this.genericFour.size();
 	}
 	
