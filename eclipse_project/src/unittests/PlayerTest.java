d72578619a4f60c2763b2bcad969db76a141d451
sushilApp <patilsushil1991@gmail.com>
2015-07-15 14:57:54 -0400

    Player class
    Added few more

--- eclipse_project/src/unittests/PlayerTest.java
+++ eclipse_project/src/unittests/PlayerTest.java
@@ -2,13 +2,81 @@ package unittests;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
+
+import eclipse_project.DedicationTokens;
+import eclipse_project.FavorTokens;
+import eclipse_project.LakeTiles;
+import eclipse_project.LanternCards;
 import eclipse_project.Player;
 public class PlayerTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	Player player;
+	
+	@Before 
+	public void testPlayer(){
+		player=new Player("DemoPlayer");
 	}
+	
+	@Test
+	public void testPickLanternCard() {
+		LanternCards lanternCardsAvailable=new LanternCards(4, 8, 7, 6, 5, 4, 3, 2);
+		assertTrue(player.pickLanternCard("redCard", lanternCardsAvailable));
+	}
+
+		
+	@Test
+	public void testPickFavorTokens(){
+		FavorTokens favorToken=new FavorTokens(20);
+		player.pickFavorTokens(favorToken);
+		assertEquals(19, favorToken.getTokens());
+	}
+	
+	@Test 
+	public void testSetAndGetLakeTiles(){
+		ArrayList<LakeTiles> playerLTStack;
+		LakeTiles l=new LakeTiles();
+		player.setLakeTiles(l);
+		playerLTStack=player.getLakeTiles();
+		assertEquals(1,playerLTStack.size());
+		
+	}
+	
+
+	@Test
+	public void testPlaceLakeTile(){
+		//Q: What is index value
+		//assertNotNull(player.placeLakeTile(37));
+	}
+
+	@Test
+	public void testSpendFavorTokens(){
+		FavorTokens favorToken=new FavorTokens(10);
+		LanternCards lanternCardsAvailable=new LanternCards(4, 7, 7, 6, 5, 4, 8, 2);
+		assertTrue(player.spendFavorTokens(favorToken, lanternCardsAvailable, "redCard", "blackCard"));
+		//System.out.println(lanternCardsAvailable.redCardCount());
+		//System.out.println(lanternCardsAvailable.blackCardCount());
+	}
+
+	@Test
+	public void testReturnLanternCards(){
+		LanternCards lanternCardsAvailable=new LanternCards(4, 8, 7, 6, 5, 4, 3, 2);
+		assertTrue(player.returnLanternCards("redCard", "blackCard", lanternCardsAvailable));
+		
+	}
+
+	@Test
+	public void testCheckFavorTokens(){
+		player.setFavorToken(20);
+		assertTrue(player.getFavorToken()==20);
+	}
+
+	
 
 }
