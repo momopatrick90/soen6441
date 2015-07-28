9f8cfc9b05bee7bd04bd64a427ce2c6d7a58fc38
sushilApp <patilsushil1991@gmail.com>
2015-07-28 10:52:51 -0400

    Final change

--- eclipse_project/src/unittests/AllTests.java
+++ eclipse_project/src/unittests/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ BoardTest.class, DedicationTokensTest.class,
-		FavorTokensTest.class, GameEngineTest.class, LakeTileTest.class,
+		FavorTokensTest.class, LakeTileTest.class,
 		LanternCardsTest.class, PlayerTest.class })
 public class AllTests {
 
